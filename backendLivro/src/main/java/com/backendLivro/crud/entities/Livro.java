da6d3ea891e77fe011352acc13968501989bca10
Gabriel Gomes <100102446+gabrxgomes@users.noreply.github.com>
2023-08-16 12:19:32 -0300

    enviando arquivos arrumados, score e category

---
+++ backendLivro/src/main/java/com/backendLivro/crud/entities/Livro.java
@@ -0,0 +1,81 @@
+package com.backendLivro.crud.entities;
+
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name = "tb_livro")
+
+public class Livro {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	private Long id;
+	private String name;
+	
+	
+	@ManyToOne
+	@JoinColumn(name = "categoria_id")
+	private Categoria categorianome;
+	
+	
+	@ManyToOne
+	@JoinColumn(name = "score_id")
+	
+	
+	private Score scorevalue;
+	
+	
+	
+	public Livro() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Categoria getCategoria() {
+		return categorianome;
+	}
+
+	public void setCategoria(Categoria categorianome) {
+		this.categorianome = categorianome;
+	}
+
+	public Score getScore() {
+		return scorevalue;
+	}
+
+	public void setScore(Score scorevalue) {
+		this.scorevalue = scorevalue;
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ backendLivro/src/main/resources/import.sql
@@ -0,0 +1,25 @@
+INSERT INTO tb_categoria(categorianome) VALUES ('Ficção')
+INSERT INTO tb_categoria(categorianome) VALUES ('Aventura');
+INSERT INTO tb_categoria(categorianome) VALUES ('Romance');
+INSERT INTO tb_categoria(categorianome) VALUES ('Terror');
+INSERT INTO tb_categoria(categorianome) VALUES ('Suspense');
+
+
+
+
+
+INSERT INTO tb_score(scorevalue) VALUES ('Alto');
+INSERT INTO tb_score(scorevalue) VALUES ('Medio');
+INSERT INTO tb_score(scorevalue) VALUES ('Baixo');
+
+
+
+
+
+
+
+INSERT INTO tb_livro(categoria_id, name, score_id) VALUES (3, 'Maria antonieta e romeu', 1);
+INSERT INTO tb_livro(categoria_id, name, score_id) VALUES (1, 'Bob lazar', 2);
+INSERT INTO tb_livro(categoria_id, name, score_id) VALUES (2, 'Perseguição no Mar', 3);
+INSERT INTO tb_livro(categoria_id, name, score_id) VALUES (4, 'O inquilino', 2);
+INSERT INTO tb_livro(categoria_id, name, score_id) VALUES (5, 'O mercenário', 1);
